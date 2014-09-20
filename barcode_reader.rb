0543e08b0bad1e2c5fa856fdf3348325f15a95a7
Mathias Rong <mathiasrong@gmail.com>
2014-09-21 00:47:17 +0200

    it works2

--- barcode_reader.rb
+++ barcode_reader.rb
@@ -4,6 +4,10 @@ require "net/http"
 require "uri"
 require 'libdevinput'
 
+@@hid = { 4 => 'a', 5 => 'b', 6 => 'c', 7 => 'd', 8 => 'e', 9 => 'f', 10 => 'g', 11 => 'h', 12 => 'i', 13 => 'j', 14 => 'k', 15 => 'l', 16 => 'm', 17 => 'n', 18 => 'o', 19 => 'p', 20 => 'q', 21 => 'r', 22 => 's', 23 => 't', 24 => 'u', 25 => 'v', 26 => 'w', 27 => 'x', 28 => 'y', 29 => 'z', 30 => '1', 31 => '2', 32 => '3', 33 => '4', 34 => '5', 35 => '6', 36 => '7', 37 => '8', 38 => '9', 39 => '0', 44 => ' ', 45 => '-', 46 => '=', 47 => '[', 48 => ']', 49 => '\\', 51 => ';' , 52 => '\'', 53 => '~', 54 => ',', 55 => '.', 56 => '/'  }
+
+@@hid2 = { 4 => 'A', 5 => 'B', 6 => 'C', 7 => 'D', 8 => 'E', 9 => 'F', 10 => 'G', 11 => 'H', 12 => 'I', 13 => 'J', 14 => 'K', 15 => 'L', 16 => 'M', 17 => 'N', 18 => 'O', 19 => 'P', 20 => 'Q', 21 => 'R', 22 => 'S', 23 => 'T', 24 => 'U', 25 => 'V', 26 => 'W', 27 => 'X', 28 => 'Y', 29 => 'Z', 30 => '!', 31 => '@', 32 => '#', 33 => '$', 34 => '%', 35 => '^', 36 => '&', 37 => '*', 38 => '(', 39 => ')', 44 => ' ', 45 => '_', 46 => '+', 47 => '{', 48 => '}', 49 => '|', 51 => ' =>' , 52 => '"', 53 => '~', 54 => '<', 55 => '>', 56 => '?'  }
+
 EVIOCGRAB = 1074021776
 DevInput.class_eval { attr_reader :dev }
 
@@ -36,7 +40,7 @@ def process_barcode(code)
 end
 
 def read_barcode2
-  @barcode_scanner = DevInput.new('/dev/hidraw0')
+  @barcode_scanner = DevInput.new('/dev/input/by-id/usb-13ba_Barcode_Reader-event-kbd')
   # Grab barcode scanner exclusively (so keypress events aren't heard by Linux)
   #@barcode_scanner.dev.ioctl(EVIOCGRAB, 1)
   @barcode_scanner.each do |event|
@@ -50,27 +54,27 @@ def read_barcode2
 end
 
 def read_barcode
-  done = false 
-  data = []
-  str = ''
-  dev = File.open("/dev/hidraw0")
+  f = open('/dev/hidraw0','rb')
+  done  = false
+  code = ''
   while !done
-    str += dev.read(4) # 1 Byte lesen, blockt wenn noch keine da sind
-    done = true if str['\rn'] != nil || str['\r'] != nil || str['\n']
-    puts str
-    #if str
-    #  data << str
-    #  puts "received char #{str}"
-    #  done == true if str == '\rn'
-    #end
-    # Daten verarbeiten
+    buf = f.read(8)
+    buf.each_char do |char|
+      c = char.ord
+      if c == 40
+        done = true
+      else
+        puts c if c > 0
+        code += @@hid[c] if c > 0
+      end 
+    end
   end
-  code = data.unpack("A").join 
-  puts "code: #{code} "
-  #process_barcode(code)
+  puts "CODE: #{code}"
 end
 
 while true
  puts "Enter barcode:"
- read_barcode2
+ read_barcode
+ #code = gets
+ #puts "Code: #{code}"
 end
